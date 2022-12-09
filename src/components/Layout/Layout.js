9ee84f0c80191ed9ca5179e38f4b7fed6858e519
jamesikjo <jamesikjo@gmail.com>
2022-12-09 13:47:48 -0800

    Remove children prop and replace with <Outlet />
    React-router v6 requires <Outlet /> component in order to render the nested child Route components.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,17 @@
 import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 //Layout Component is a wrapper for all pages
-//It will display Header - Content(children components) - Footer on all pages
+//It will display Header - Content(children <Outlet />) - Footer on all pages
 //<Layout /> is used in App.js
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main>
+        <Outlet /> {/* <-- nested children routes rendered here! */}
+      </main>
       {/* Footer Component Here */}
     </>
   );
