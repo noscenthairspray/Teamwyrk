5c4ed9785d410131f55d67d9bc350ca978893f48
jamesikjo <jamesikjo@gmail.com>
2022-12-13 14:47:00 -0800

    Update styles for about page

--- src/pages/About/components/Showcase/Statement/Statement.js
+++ src/pages/About/components/Showcase/Statement/Statement.js
@@ -6,12 +6,18 @@ const Statement = ({ statement, position }) => {
   const stylePosition = position % 2 === 0 ? "Even" : "Odd";
 
   return (
-    <div className={styles[`container${stylePosition}`]}>
+    <div
+      className={`${styles.container} ${styles[`container${stylePosition}`]}`}
+    >
       <div className={styles.wrapperText}>
         <h2>{title}</h2>
         <p>{text}</p>
       </div>
-      <div className={styles[`wrapperEllipse${stylePosition}`]}>
+      <div
+        className={`${styles.wrapperEllipse} ${
+          styles[`wrapperEllipse${stylePosition}`]
+        }`}
+      >
         <img src={image} alt={title} />
       </div>
     </div>
