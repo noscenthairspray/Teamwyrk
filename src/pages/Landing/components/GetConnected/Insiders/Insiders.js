d10c8c7df77530be615053266079f75403b30f4d
jamesikjo <jamesikjo@gmail.com>
2023-01-17 13:01:52 -0800

    Update global css prop to local and styling

--- src/pages/Landing/components/GetConnected/Insiders/Insiders.js
+++ src/pages/Landing/components/GetConnected/Insiders/Insiders.js
@@ -1,25 +1,26 @@
 import styles from "./Insiders.module.css";
 
-const Insiders = ( { insider, position}) => {
+const Insiders = ({ insider, position }) => {
+  const { name, company, role, image } = insider;
 
-    const {name, description, image } = insider;
-
-    return (
-        <div className = {styles.container}>
-
-            <div>
-                <img src={image} alt="insiders"></img>
-            </div>
-            <div className = {styles.wrapperText}>
-                <div className = {styles.name}>
-                    <p>{name}</p>
-                </div>
-                <div className = {styles.description}>
-                    <p>{description}</p>
-                </div>  
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageWrapper}>
+        <img src={image} alt="insiders"></img>
+      </div>
+      <div className={styles.wrapperText}>
+        <div className={styles.name}>
+          <p>{name}</p>
         </div>
-    );
+        <div className={styles.company}>
+          <p>{company}</p>
+        </div>
+        <div className={styles.role}>
+          <p>{role}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Insiders;
