096deaa60c045abb6a5cd202553211d21e866b06
jamesikjo <jamesikjo@gmail.com>
2023-01-17 12:54:44 -0800

    Add anchor tags and update styling to fix layout

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,22 +1,29 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <footer className = {styles.container}>
-            <div className = {styles.upperContainer}>
-                <div className = {styles.imageWrapper}>
-                    <img src = {"images/landing_page/linkedin.svg"} alt = "linkedin"></img>
-                </div>
-            </div>
+  return (
+    <footer className={styles.container}>
+      <div className={styles.upperContainer}>
+        <div className={styles.imageWrapper}>
+          {/* TODO: add teamwyrk linkedIn path */}
+          <a href="#">
+            <img src={"images/landing_page/linkedin.svg"} alt="linkedin"></img>
+          </a>
+        </div>
+      </div>
 
-            <div className = {styles.lowerContainer}>
-                <div className = {styles.textWrapper}>
-                    <b className = {styles.website}> team@teamwryk.com </b>
-                    <b className = {styles.copyright}> © 2022 TeamWyrk. All rights reserved.</b>
-                </div>
-            </div>
-        </footer>
-    );
+      <div className={styles.lowerContainer}>
+        <div className={styles.textWrapper}>
+          <a href="mailto: team@teamwryk.com" className={styles.website}>
+            team@teamwryk.com
+          </a>
+          <p className={styles.copyright}>
+            © 2022 TeamWyrk. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
