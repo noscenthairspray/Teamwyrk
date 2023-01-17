ff422e1b16909fad4c2d72886a5982e50eee8adb
jamesikjo <jamesikjo@gmail.com>
2023-01-17 12:56:18 -0800

    Split description property to company and role

--- src/pages/Landing/data.js
+++ src/pages/Landing/data.js
@@ -1,17 +1,20 @@
 export const insiders = [
-    {
-        name: "Frank Lapinski",
-        description: "Microsoft Product Manager",
-        image: "/images/landing_page/frank_avatar.png",
-    },
-    {
-        name: "Arron Watson",
-        description: "Betterment CX Associate",
-        image: "/images/landing_page/arron_avatar.png",
-    },
-    {
-        name: "Al Wang",
-        description: "DocuSign Product Manager",
-        image: "/images/landing_page/al_avatar.png",
-    },
+  {
+    name: "Frank Lapinski",
+    company: "Microsoft",
+    role: "Product Manager",
+    image: "/images/landing_page/frank_avatar.png",
+  },
+  {
+    name: "Arron Watson",
+    company: "Betterment",
+    role: "CX Associate",
+    image: "/images/landing_page/arron_avatar.png",
+  },
+  {
+    name: "Al Wang",
+    company: "DocuSign",
+    role: "Product Manager",
+    image: "/images/landing_page/al_avatar.png",
+  },
 ];
