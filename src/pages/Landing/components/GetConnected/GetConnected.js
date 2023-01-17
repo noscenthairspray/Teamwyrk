5668d1beed6443135892956c9feabb322a54977f
jamesikjo <jamesikjo@gmail.com>
2023-01-17 12:59:20 -0800

    Update mapped Insider props

--- src/pages/Landing/components/GetConnected/GetConnected.js
+++ src/pages/Landing/components/GetConnected/GetConnected.js
@@ -2,27 +2,20 @@ import Insiders from "./Insiders";
 import styles from "./GetConnected.module.css";
 import { insiders } from "../../data";
 
-
 const GetConnected = () => {
-    return (
-        <div className = {styles.container}>
-            <div className = {styles.title}>
-                <p> Get connected to insiders from top companies </p>
-            </div>
-            <div className = {styles.wrapperText}>
-                {insiders.map((insider, index) => (
-                    <Insiders insider={insider} position = {index} />
-                ))}
-            </div>
-            <div>
-                <button className={styles.secondaryButton}>
-                    <p className = {styles.buttonText}>
-                        Become an Insider
-                    </p>
-                    </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <p> Get connected to insiders from top companies </p>
+      </div>
+      <div className={styles.wrapperText}>
+        {insiders.map((insider, index) => (
+          <Insiders insider={insider} position={index} key={insider.name} />
+        ))}
+      </div>
+      <button className={styles.secondaryButton}>Become an Insider</button>
+    </div>
+  );
 };
 
 export default GetConnected;
