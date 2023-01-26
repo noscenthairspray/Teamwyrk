8dbccb1155f44c3d2762dcb14fb2136346cdae40
jamesikjo <jamesikjo@gmail.com>
2023-01-26 15:39:57 -0800

    Create Sign Up and Sign In pages

---
+++ src/pages/SignIn/SignIn.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Credentials } from "../../components/Forms";
+
+import styles from "./SignIn.module.css";
+
+const SignIn = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftHalf} />
+      <div className={styles.rightHalf}>
+        <Credentials title="Sign in to WyrkTeam" submitBtnText="Sign in" />
+
+        <div className={styles.dividerWrapper}>
+          <p className={styles.divider}>or</p>
+        </div>
+
+        <button className={styles.googleButton}>
+          <img src="images/access/google_icon.svg" alt="Google Logo" />
+          Sign in with Google
+        </button>
+        <p className={styles.helperText}>
+          {`Don't have an account yet? `}
+          <a href="/signup">Sign up</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
