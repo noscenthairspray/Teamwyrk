982e0239b0e5720b40283ab70830d3e95cfd5e7d
Yuejia Tong <101756281+yuejiat-0416@users.noreply.github.com>
2023-02-01 12:09:10 -0500

    update partial dynamic header

--- src/components/Layout/ActiveHeader/ActiveHeader.js
+++ src/components/Layout/ActiveHeader/ActiveHeader.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./ActiveHeader.module.css";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
+/*
 const ActiveHeader = () => {
   return (
     <header>
@@ -32,5 +42,55 @@ const ActiveHeader = () => {
     </header>
   );
 };
+*/
+
+
+const ActiveHeader = () => {
+  /*
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleListItemClick = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    index: number,
+  ) => {
+    setSelectedIndex(index);
+  };
+  */
+
+  return (
+    <nav className={styles.navContainer}>
+      <List component="nav">
+        <ListItemButton
+          selected={selectedIndex === 0}
+          onClick={(event) => handleListItemClick(event, 0)}
+        >
+          <ListItemText primary="Request Feed" />
+        </ListItemButton>
+
+        <ListItemButton
+          selected={selectedIndex === 1}
+          onClick={(event) => handleListItemClick(event, 1)}
+        >
+          <ListItemText primary="About" />
+        </ListItemButton>
+
+        <ListItemButton
+          selected={selectedIndex === 2}
+          onClick={(event) => handleListItemClick(event, 2)}
+        >
+          <ListItemText primary="FAQ" />
+        </ListItemButton>
+
+        <ListItemButton
+          selected={selectedIndex === 3}
+          onClick={(event) => handleListItemClick(event, 3)}
+        >
+          <ListItemText primary="Contact Us" />
+        </ListItemButton>
+
+      </List>
+    </nav>
+  );
+}
 
 export default ActiveHeader;
