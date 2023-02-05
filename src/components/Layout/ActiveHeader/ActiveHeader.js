d6b246f0c6d6490f521282019f7a18d7cc52705b
Yuejia Tong <101756281+yuejiat-0416@users.noreply.github.com>
2023-02-05 22:03:21 +0000

    Update for dynamic header

--- src/components/Layout/ActiveHeader/ActiveHeader.js
+++ src/components/Layout/ActiveHeader/ActiveHeader.js
@@ -1,16 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./ActiveHeader.module.css";
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+import Button from '@mui/material/Button';
 
-/*
 const ActiveHeader = () => {
   return (
     <header>
@@ -24,73 +15,35 @@ const ActiveHeader = () => {
         <div className={styles.linkWrapper}>
           <ul className={styles.navLinks}>
             <li>
-                <Link to="/request/form"> Request Feed</Link> 
+                <Link to="/request/form">
+                  <Button variant="text">Request Feed</Button>
+                </Link> 
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">
+                <Button variant="text">About</Button>
+              </Link>
             </li>
             <li>
-              <Link to="/faq">FAQ</Link>
+              <Link to="/faq">
+                <Button variant="text">FAQ</Button>
+              </Link>
             </li>
             <li>
-              <Link to="/contact">Contact Us</Link>
+              <Link to="/contact">
+                <Button variant="text">Contact Us</Button>
+              </Link>
             </li>
           </ul>
+        
+        <div className={styles.accessButtons}>
+          
+        </div>
 
         </div>
       </nav>
     </header>
   );
 };
-*/
-
-
-const ActiveHeader = () => {
-  /*
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-  const handleListItemClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number,
-  ) => {
-    setSelectedIndex(index);
-  };
-  */
-
-  return (
-    <nav className={styles.navContainer}>
-      <List component="nav">
-        <ListItemButton
-          selected={selectedIndex === 0}
-          onClick={(event) => handleListItemClick(event, 0)}
-        >
-          <ListItemText primary="Request Feed" />
-        </ListItemButton>
-
-        <ListItemButton
-          selected={selectedIndex === 1}
-          onClick={(event) => handleListItemClick(event, 1)}
-        >
-          <ListItemText primary="About" />
-        </ListItemButton>
-
-        <ListItemButton
-          selected={selectedIndex === 2}
-          onClick={(event) => handleListItemClick(event, 2)}
-        >
-          <ListItemText primary="FAQ" />
-        </ListItemButton>
-
-        <ListItemButton
-          selected={selectedIndex === 3}
-          onClick={(event) => handleListItemClick(event, 3)}
-        >
-          <ListItemText primary="Contact Us" />
-        </ListItemButton>
-
-      </List>
-    </nav>
-  );
-}
 
 export default ActiveHeader;
