541fcd04839e788b2d5409a1836e217f7db83934
Yuejia Tong <101756281+yuejiat-0416@users.noreply.github.com>
2023-02-05 22:35:38 +0000

    update Dynamic Header

--- src/components/Layout/ActiveHeader/ActiveHeader.js
+++ src/components/Layout/ActiveHeader/ActiveHeader.js
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom";
 import styles from "./ActiveHeader.module.css";
 import Button from '@mui/material/Button';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+
+
+
+const settings = ['Account Settings', 'Log Out'];
 
 const ActiveHeader = () => {
+
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <header>
       <nav className={styles.navContainer}>
@@ -37,6 +59,34 @@ const ActiveHeader = () => {
           </ul>
         
         <div className={styles.accessButtons}>
+        <Box sx={{ flexGrow: 0}}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem color= "yellow" key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
           
         </div>
 
