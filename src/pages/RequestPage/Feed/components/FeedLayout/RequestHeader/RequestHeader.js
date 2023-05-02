d15b8f07c037930ea90888d01b7d34347de41cda
Rafael Villa <villalugo96@gmail.com>
2023-05-01 21:48:38 -0400

    added empty request screen

--- src/pages/RequestPage/Feed/components/FeedLayout/RequestHeader/RequestHeader.js
+++ src/pages/RequestPage/Feed/components/FeedLayout/RequestHeader/RequestHeader.js
@@ -3,14 +3,19 @@ import StyledButton from "../../../../../../components/StyledButton";
 import { Header } from "../../../../../../components/Typography";
 import styles from "./RequestHeader.module.css";
 
-const RequestHeader = () => {
+const RequestHeader = ({ hideButton }) => {
+  const buttonClass = hideButton ? styles.hideButton : styles.styledButton ;
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <Header color="darkBlue">Requests</Header>
       </div>
       <Link to="/request/form">
-        <StyledButton color="yellow" hover>
+        <StyledButton
+         className={buttonClass}
+          color="yellow"
+          hover
+        >
           Submit a request
         </StyledButton>
       </Link>
