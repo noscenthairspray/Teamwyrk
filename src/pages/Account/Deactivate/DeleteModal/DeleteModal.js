2ea28cfc10712bbb7bc7e7a2b863219b760f39cc
Lakshay989 <lakshaykucheriya@gmail.com>
2023-07-26 23:16:37 -0600

    Implemented pop-up on click Delete Account

---
+++ src/pages/Account/Deactivate/DeleteModal/DeleteModal.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import styles from "./DeleteModal.module.css";
+
+const Modal = ({ closeModal }) => {
+  console.log("Entering Modal");
+  console.log(closeModal);
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContainer}>
+        <div className={styles.titleCloseBtn}>
+          <button
+            onClick={() => {
+              closeModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className={styles.title}>
+          <h1>This is a test Modal </h1>
+        </div>
+        <div className={styles.body}>
+          <p>The continue functionality has not been implemented.</p>
+        </div>
+        <div className={styles.footer}>
+          <button
+            className={styles.cancelBtn}
+            onClick={() => closeModal(false)}
+          >
+            Cancel
+          </button>
+          <button className={styles.continueBtn}> Continue </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
