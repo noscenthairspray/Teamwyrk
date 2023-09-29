96b7b7dd0d119eea28bac0fc5a74df66c04a75cd
Marilia Henriquez <94565577+mariliah@users.noreply.github.com>
2023-09-29 15:13:37 -0500

    Update HeroItem.js
    change words on landing page

--- src/pages/Landing/components/HeroItem/HeroItem.js
+++ src/pages/Landing/components/HeroItem/HeroItem.js
@@ -59,8 +59,7 @@ const HeroItem = () => {
           ) : (
             <>
               <p className={styles.textBodyTop}>
-                We’ll be your team, connecting you with insiders at top <br />
-                companies to land your next role at no extra cost.
+                Our app connects users with insiders at tech companies.
               </p>
             </>
           )}
