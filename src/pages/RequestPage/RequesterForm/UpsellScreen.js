e6dc62c65a3c507d7bfbd5e399a0e72f651749de
Lakshay989 <lakshaykucheriya@gmail.com>
2023-10-12 02:39:49 -0600

    Working on the Upsell Screen

---
+++ src/pages/RequestPage/RequesterForm/UpsellScreen.js
@@ -0,0 +1,20 @@
+// Upsell Screen for Request Confirmation
+
+
+export const UpsellScreen = () => {
+
+    return(
+        <div>
+        <p> Request Submitted !</p>
+        <p> Your request has been submitted successfully. You will receive an email notification once an Insider accepts your request and matches with you.</p>
+    </div>
+    )
+
+}
+
+// return
+// (
+//     // Request Submitted Modal
+//     // CardItems
+//     //Button for going back
+// )
