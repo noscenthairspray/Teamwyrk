12dfcdc120251bc4fdb47da1bbb5bf786ca3c3a6
Van Tu <vtu7410@gmail.com>
2024-01-22 11:03:08 -0800

    Fixed email styling

--- src/pages/RequestPage/RequesterFeed/components/RequestItem/InsiderAcceptModal/DeclinedEmailTemplate.js
+++ src/pages/RequestPage/RequesterFeed/components/RequestItem/InsiderAcceptModal/DeclinedEmailTemplate.js
@@ -1,29 +1,39 @@
 const DeclinedEmailTemplate = (sender, target) => {
   return `
-      <div style="font-family: Arial, sans-serif; color: #333;">
-        <div style="text-align: center; margin-bottom: 30px; color: #333;">
-          <h2>TeamWyrk</h2>
-          <small>built by Bootcamp2</small>
-        </div>
-        <div style="background-color: #f9f9f9; padding: 20px;">
-          <h1>You have been declined</h1>
-          <p>Hi ${target},</p>
-          <p>
-             You are receiving this email to inform you that ${sender} has
-          declined your help. The request will be removed from your feed.
-          </p>
-          <p>Best,<br>TeamWyrk</p>
-        </div>
-        <div style="text-align: center; margin-top: 20px;">
-          <a href="https://www.teamwyrk.org/accept-insider" 
-             style="background-color: #4C9AFF; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">
-            Accept your Insider
-          </a>
-        </div>
-        <div style="text-align: center; margin-top: 30px;">
-          <p>Have a question? <a href="https://www.teamwyrk.org/support" style="color: #4C9AFF;">Contact our support team</a>.</p>
-        </div>
-      </div>
+  <div style="color: #f6f7fb">
+  <div style="text-align: center; margin-bottom: 30px; color: #eff2fc">
+    <h2 style="color: #37447e; font-family: Roboto">TeamWyrk</h2>
+    <small style="color: #37447e; font-family: PT Sans"
+      >built by Bootcamp2</small
+    >
+  </div>
+  <div
+    style="background-color: #f9f9f9; padding: 20px; font-family: PT Sans"
+  >
+    <h1 style="color: #37447e">You have been declined</h1>
+    <p style="color: #202124">Hi ${target},</p>
+    <p style="color: #202124">
+      You are receiving this email to inform you that ${sender} has declined
+      your help. The request will be removed from your feed.
+    </p>
+    <p style="#000">Best,<br />TeamWyrk</p>
+  </div>
+  <div
+    style="
+      text-align: center;
+      margin-top: 30px;
+      background-color: #eff2fc;
+      font-family: Roboto;
+    "
+  >
+    <p style="color: #202124">
+      Have a question?
+      <a href="https://www.teamwyrk.org/support" style="color: #05f"
+        >Contact our support team</a
+      >.
+    </p>
+  </div>
+</div>
     `;
 };
 
