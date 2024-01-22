4162ad17432b4cd6f6b04b9165061570cdb9cb60
LinkdJulioFlores <julio.cesar.2000@hotmail.com>
2024-01-21 21:38:39 -0500

    Email is visual compliant TM-80

--- src/pages/RequestPage/InsiderFeed/components/GetMatchedModal/ContinueEmailTemplate.js
+++ src/pages/RequestPage/InsiderFeed/components/GetMatchedModal/ContinueEmailTemplate.js
@@ -1,30 +1,30 @@
 const emailTemplate = (sender, target) => {
   return `
-    <div style="font-family: Arial, sans-serif; color: #333;">
-      <div style="text-align: center; margin-bottom: 30px; color: #333;">
-        <h2>TeamWyrk</h2>
-        <small>built by Bootcamp2</small>
+    <div style="font-family: PT Sans Bold 14pt, sans-serif; color: #333;">
+      <div style="text-align: left; margin-bottom: 30px; padding: 23px 24px; color: #37447e; background-color: #eff2fc;">
+        <h2 style="display: inline;">TeamWyrk</h2>
+        <small style="display: inline;">built by Bootcamp2</small>
       </div>
-      <div style="background-color: #f9f9f9; padding: 20px;">
+      <div style="padding: 20px;">
         <h1>${sender} has decided to help you!</h1>
         <p>Hi ${target},</p>
         <p>${sender} is interested in helping you fulfill your request.
-           Please navigate back to Teamwyrk to view ${sender} information and
-           <strong>make a decision</strong>.
+          Please navigate back to Teamwyrk to view ${sender} information and
+          <strong>make a decision</strong>.
         </p>
         <p>Best,<br>TeamWyrk</p>
       </div>
       <div style="text-align: center; margin-top: 20px;">
         <a href="https://www.teamwyrk.org/accept-insider" 
-           style="background-color: #4C9AFF; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">
+          style="background-color: #37447e; color: #ffffff; padding: 23px 24px; text-decoration: none; border-radius: 9px; display: inline-block;">
           Accept your Insider
         </a>
       </div>
-      <div style="text-align: center; margin-top: 30px;">
+      <div style="text-align: center; margin-top: 30px; padding: 34px 30px; background-color: #eff2fc;">
         <p>Have a question? <a href="https://www.teamwyrk.org/support" style="color: #4C9AFF;">Contact our support team</a>.</p>
       </div>
-    </div>
+    </div> 
   `;
-  };
-  
-  export default emailTemplate;
+};
+
+export default emailTemplate;
