cfc006ff1d9a2cf15322cafa90acb8f4a6bb6f4d
Van Tu <vtu7410@gmail.com>
2024-01-26 15:13:09 -0800

    Added deactivation email features

--- src/pages/Account/Deactivate/Deactivate.js
+++ src/pages/Account/Deactivate/Deactivate.js
@@ -1,15 +1,42 @@
 import { useState } from "react";
 import styles from "./Deactivate.module.css";
 import Modal from "./DeleteModal";
+import DeactivateEmailTemplate from "./DeactiviateEmailTemplate";
+import { db } from "../../../firebase";
+import { addDoc, collection } from "firebase/firestore";
 
 //Deactivate + Delete Account te
 const Deactivate = ({ user }) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const handleDeactivate = async () => {
+    const emailTemplate = DeactivateEmailTemplate(user.displayName);
+
+    try {
+      // Adds new email document
+      const docRef = await addDoc(collection(db, "mail"), {
+        to: user.email,
+        message: {
+          subject: "Your Account has been deactivated",
+          html: emailTemplate,
+        },
+      });
+    } catch (error) {
+      // Error sending email
+    }
+  };
   return (
     <>
       <div className={styles.container}>
         <div className={styles.deactivateWrapper}>
-          <div className={styles.title}>Deactivate Account</div>
+          <button
+            className={styles.title}
+            onClick={() => {
+              handleDeactivate();
+            }}
+          >
+            Deactivate Account
+          </button>
           <p className={styles.text}>
             Need a break? We understand. If you are sure you want to deactivate
             your account, you may do so by selecting the link above.
