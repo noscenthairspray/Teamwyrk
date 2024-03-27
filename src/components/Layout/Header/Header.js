5c6dc6567b8c3a710303c138888232e0c33525b0
Kamata954 <kimani.walker954@gmail.com>
2024-03-27 02:01:34 -0400

    added message tab bubble front end

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -95,7 +95,15 @@ const Header = () => {
           {!isMobile && (
             <div className={styles.accessButtons}>
               {isAuthenticated ? (
+                <>
+                <Link className={styles.messageBox}>
+                  <svg className={styles.messageBubble} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M6 6.5H14M6 10.5H12M16 1.5C16.7956 1.5 17.5587 1.81607 18.1213 2.37868C18.6839 2.94129 19 3.70435 19 4.5V12.5C19 13.2956 18.6839 14.0587 18.1213 14.6213C17.5587 15.1839 16.7956 15.5 16 15.5H11L6 18.5V15.5H4C3.20435 15.5 2.44129 15.1839 1.87868 14.6213C1.31607 14.0587 1 13.2956 1 12.5V4.5C1 3.70435 1.31607 2.94129 1.87868 2.37868C2.44129 1.81607 3.20435 1.5 4 1.5H16Z" stroke="#37447E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                  <div className={styles.messageAlert}>3</div>
+                </Link>
                 <ProfileDropDown user={user} />
+                </>
               ) : (
                 <>
                   <Link to="/signin">
